c95229f59e1e183fdf7f7d99f97223dc173767f0
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 23:00:28 +0700

    add validates name and email in model Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,7 @@
 
 class Customer < ApplicationRecord
   has_many :orders
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
