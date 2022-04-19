7c314e6f02b57247859ac57bd700ab1787f9c3e4
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 22:19:43 +0700

    add method get_category_by_id in model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Item < ApplicationRecord
+class Item < ApplicationRecord # rubocop:disable Style/Documentation
   has_many :order_details
   has_many :orders, through: :order_details
   has_many :item_categories
@@ -9,4 +9,9 @@ class Item < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :description, length: { maximum: 150 }
+
+  def self.get_category_by_id(id)
+    get_index_category = joins(:item_categories).where(item_categories: { item_id: id }).pluck(:category_id)
+    Category.where(id: get_index_category)
+  end
 end
