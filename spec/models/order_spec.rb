d274c370eb7791b104d9601e470687f5ab8ea2ad
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 23:06:33 +0700

    add test model Order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:order) { FactoryBot.build(:order) }
+
+  describe 'validations' do
+    it 'is valid with attributes' do
+      customer = FactoryBot.create(:customer)
+      order = FactoryBot.build(:order, customer_id: customer.id)
+      expect(order).to be_valid
+    end
+
+    it 'is invalid without a customer' do
+      order.customer_id = nil
+      expect(order).to_not be_valid
+    end
+
+    it 'is invalid without a status' do
+      order.status = nil
+      expect(order).to_not be_valid
+    end
+
+    it 'is invalid without a total' do
+      order.total = nil
+      expect(order).to_not be_valid
+    end
+  end
 end
