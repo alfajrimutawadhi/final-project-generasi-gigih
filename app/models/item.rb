a8b256a2dfc7d952727cbe9d6d191f94fdc3fe70
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 22:17:35 +0700

    add validates in model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   has_many :orders, through: :order_details
   has_many :item_categories
   has_many :categories, through: :item_categories
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :description, length: { maximum: 150 }
 end
