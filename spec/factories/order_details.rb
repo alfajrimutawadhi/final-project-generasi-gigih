23c8bd69dcab99ffa8b426cf86b44520bb53b5cb
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 23:12:22 +0700

    add test model OrderDetail

--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :order_detail do
     quantity { 1 }
     item_price { 1.5 }
+    order_id { 1 }
+    item_id { 1 }
   end
 end
--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe OrderDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @item = FactoryBot.create(:item, description: 'Special Food')
+    @customer = FactoryBot.create(:customer)
+    @order = FactoryBot.create(:order, customer_id: @customer.id)
+  end
+  subject(:order_detail) { FactoryBot.build(:order_detail) }
+
+  describe 'validations' do
+    it 'is valid with attributes' do
+      expect(order_detail).to be_valid
+    end
+  end
 end
