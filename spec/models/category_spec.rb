d16c01864c03336347cd7432e2246c17cf190965
alfajrimutawadhi <alfajrimutawadhi@gmail.com>
2022-04-19 22:54:12 +0700

    add test model Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:category) { FactoryBot.build(:category) }
+
+  describe 'validations' do
+    it 'is invalid without a name' do
+      category.name = nil
+      expect(category).to_not be_valid
+    end
+
+    it 'is valid with a name' do
+      category.name = 'Main Dish'
+      expect(category).to be_valid
+    end
+
+    it 'is invalid with duplicate name' do
+      category1 = Category.create(name: 'Main Dish') # rubocop:disable Lint/UselessAssignment
+      category2 = Category.new(name: 'Main Dish')
+      expect(category2).to_not be_valid
+    end
+  end
 end
